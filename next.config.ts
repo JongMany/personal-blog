920583f50f0c0bccb16dcf1f89d58e5e65307979
JongMany <blackberry1114@naver.com>
2026-05-24 02:43:39 +0900

    chore: 빌드 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -3,27 +3,8 @@ import type { NextConfig } from "next";
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-class VeliteWebpackPlugin {
-  static started = false;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  apply(compiler: any) {
-    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-      if (VeliteWebpackPlugin.started) return;
-      VeliteWebpackPlugin.started = true;
-      const dev = compiler.options.mode === "development";
-      const { build } = await import("velite");
-      await build({ watch: dev, clean: !dev });
-    });
-  }
-}
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  webpack: (config: any) => {
-    config.plugins.push(new VeliteWebpackPlugin());
-    return config;
-  },
 };
 
 export default withVanillaExtract(nextConfig);
